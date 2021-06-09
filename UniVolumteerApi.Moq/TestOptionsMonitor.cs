3de27e0bad4bad9030ffd5a9f0d3ad6905eb9912
Sergey Patoka <sergey.pat98@gmail.com>
2021-06-09 09:04:17 +0500

    Тестовый монитор настроек

---
+++ UniVolumteerApi.Moq/TestOptionsMonitor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+
+namespace UniVolunteerApi.Moq
+{
+    public class TestOptionsMonitor<T> : IOptionsMonitor<T>
+    {
+        public TestOptionsMonitor(T inner)
+        {
+            this.inner = inner;
+        }
+
+
+        private T inner { get; }
+
+        public T CurrentValue => inner;
+
+        public T Get(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable OnChange(Action<T, string> listener)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
