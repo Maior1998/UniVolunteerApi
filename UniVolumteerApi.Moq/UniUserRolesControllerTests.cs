e0d3610115bf15f3b11f56cf904d3734e05b3732
DanaAkp <aidnagul10@gmail.com>
2021-06-09 09:04:52 +0500

    CreateUserRoleTest

--- UniVolumteerApi.Moq/UniUserRolesControllerTests.cs
+++ UniVolumteerApi.Moq/UniUserRolesControllerTests.cs
@@ -7,12 +7,26 @@ using System.Threading.Tasks;
 using UniVolunteerApi.Repositories;
 using UniVolunteerApi.Services;
 using Moq;
+using UniVolunteerApi.Controllers;
+using Xunit;
 
 namespace UniVolunteerApi.Moq
 {
-    class UniUserRolesControllerTests
+    public class UniUserRolesControllerTests
     {
+        private readonly UniUserRolesController _uniUserRolesController;
         private readonly Mock<IUniRepository> _repository = new Mock<IUniRepository>();
-        private readonly IUniVolunteerSession _session;
+        private readonly Mock<IUniVolunteerSession> _session = new Mock<IUniVolunteerSession>();
+
+        public UniUserRolesControllerTests()
+        {
+            _uniUserRolesController = new UniUserRolesController(_repository.Object, _session.Object);
+        }
+
+        [Fact]
+        public void CreateUserRoleTest()
+        {
+
+        }
     }
 }
